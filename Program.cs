9bb79e7dbe5dcb295718d5352fba59e53f07d892
Enzo Soares <soaressenzo@gmail.com>
2022-06-14 12:55:35 -0300

    Created a reverse string

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Question_5{
+
+    public class Program{
+        static void Main(){
+            string? text;     
+            System.Console.WriteLine("What do you want to invert?");
+            text = Console.ReadLine();     
+            while(text == " "){
+                System.Console.WriteLine("What do you want to invert?");
+            text = Console.ReadLine();
+            }
+            int size = text.Length; 
+            List<char> outText = new List<char>(); 
+            foreach(char chars in text){                
+                outText.Insert(0,chars);
+            }
+            text = new String(outText.ToArray());
+            System.Console.WriteLine(text);
+
+        }
+    }
+}
